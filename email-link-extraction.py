6ea79cf8381a584b815b55a9b3056cea257c0f8d
William Kariampuzha <wzkariampuzha@gmail.com>
2022-04-27 00:16:52 -0500

    newly-saved

---
+++ email-link-extraction.py
@@ -0,0 +1,25 @@
+#beautifulsoup4==4.8.0
+#fake-useragent==0.1.11
+#googlesearch-python==2.0.2
+#lxml==4.4.1
+#soupsieve==1.9.3
+
+from googlesearch import search
+import re
+from bs4 import BeautifulSoup
+#from socket import timeout
+#import http
+#import urllib.request
+#from urllib.request import URLError, HTTPError
+#from urllib.error import HTTPError, URLError
+#import random
+#import os
+#import time
+#import sys
+#from fake_useragent import UserAgent
+#from socket import timeout
+
+search("Google", num_results=100)
+
+if __name__ == '__main__':
+    pass
